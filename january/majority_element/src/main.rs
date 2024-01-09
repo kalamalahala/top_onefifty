094f0f232023ca2dba28944c2ba9dbdfebfe7793
kalamalahala <solo.driver.bob@gmail.com>
2024-01-08 23:21:24 -0500

    i used a hash map for this, maybe overkill

--- january/majority_element/src/main.rs
+++ january/majority_element/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let nums = vec![2,2,1,1,1,2,2];
+    let result = Solution::majority_element(nums);
+    println!("result = {}", result);
+
+    let nums = vec![3,2,3];
+    let result = Solution::majority_element(nums);
+    println!("result = {}", result);
+}
+
+struct Solution {}
+impl Solution {
+    pub fn majority_element(nums: Vec<i32>) -> i32 {
+        use std::collections::HashMap;
+        let mut counts: HashMap<i32, i32> = HashMap::with_capacity(nums.len());
+
+        for num in &nums {
+            // iterate over the vector and count the number of times each element appears
+            let count = counts.entry(*num).or_insert(0);
+            // increment the count inside the hashmap
+            *count += 1;
+        }
+
+        counts.into_iter().max_by_key(|&(_, count)| count).unwrap().0
+    }
 }
